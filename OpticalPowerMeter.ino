4b4bd6cc71f74a1c81cd2af0d8cc4b8dd43d4d5d
Frederic Marcotte <frederic.marcotte.2@ulaval.ca>
2023-02-28 16:10:20 -0500

    arduino code

--- OpticalPowerMeter.ino
+++ OpticalPowerMeter.ino
@@ -1,11 +1,59 @@
 
+// analog pins
+const int analogPins[8] = {A7,A6,A5,A4,A3,A2,A1,A0};
+
+// digital pins
+const int digitalPins[10] = {11,10,9,8,7,6,5,4,3,2};
+
+// sensor values
+int sensorValues[80];
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+
+    // initialize serial communication at 9600 bps
+    Serial.begin(9600);
+
+    // disable columns
+    for (int digitalPin: digitalPins) {
+        pinMode(digitalPin, OUTPUT);
+        digitalWrite(digitalPin, LOW);
+    }
+
+    Serial.println("Start")
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+
+    // send data only when available
+    if (Serial.available() > 0) {
+
+        // empty serial receive buffer
+        Serial.read();
+
+        // sensorValues index
+        int i = 0;
+
+        // columns loop
+        for (int digitalPin: digitalPins) {
+
+            // enable column
+            digitalWrite(digitalPin, HIGH);
+
+            // rows loop
+            for (int analogPin: analogPins) {
+                
+                // read temperature sensor
+                sensorValues[i] = analogRead(analogPin);
+                i++;
+
+            }
+
+            // disable column
+            digitalWrite(digitalPin, LOW);
+
+        }
+
+        // write sensor values
+        Serial.write(bite(sensorValues), 160);
+    }
 }
